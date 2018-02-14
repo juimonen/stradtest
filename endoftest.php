a7b21ca474d678b64de58e3222cc8b9141266626
Jaska Uimonen <jaska.uimonen@helsinki.fi>
2018-02-14 10:50:45 +0200

    fix semicolons to the end of php echos

--- endoftest.php
+++ endoftest.php
@@ -1,7 +1,7 @@
  <?php
         if(isset($_POST['submit1']) || isset($_POST['submit2'])) {
 
-           $config = parse_ini_file('../../stradtest_config.ini.php');
+           $config = parse_ini_file('../../stradtest_config.ini');
            $connection = mysqli_connect('localhost', $config['username'], $config['password'], $config['dbname']);
 
            if (mysqli_connect_errno()) {
@@ -89,72 +89,73 @@
 
 	readfile("thankyou_header.html");
 	
-	echo "Your violin evaluation results from worst to best:"
-	echo "<br><br>"
-
+	echo "Your violin evaluation results from worst to best:";
+	echo "<br><br>";
+        
 	foreach($pieces as &$value) {
 		switch ($value) {
-    	       	       case 'D00':
+    	       	       case "D00":
                	       	    echo "Stradivari 1679 Hellier";
-			    echo "<br>"
+			    echo "<br>";
         	    	    break;
-		       case 'D01':
+		       case "D01":
                	       	    echo "Stradivari 1690 Auer";
-			    echo "<br>"
+			    echo "<br>";
         	    	    break;
-    	       	       case 'D02':
+    	       	       case "D02":
                	       	    echo "Stradivari 1692 Oliveira";
-			    echo "<br>"
+			    echo "<br>";
         	    	    break;
-		       case 'D03':
+		       case "D03":
                	       	    echo "Stradivari 1701 Dushkin";
-			    echo "<br>"
+			    echo "<br>";
         	    	    break;
-		       case 'D04':
+		       case "D04":
                	       	    echo "Stradivari 1707 La Cathedrale";
-			    echo "<br>"
+			    echo "<br>";
         	    	    break;
-		       case 'D05':
+		       case "D05":
                	       	    echo "Stradivari 1708 Ruby";
-			    echo "<br>"
+			    echo "<br>";
         	    	    break;
-		       case 'D06':
+		       case "D06":
                	       	    echo "Stradivari 1710 Vieuxtemps, Hauser";
-			    echo "<br>"
+			    echo "<br>";
         	    	    break;
-		       case 'D07':
+		       case "D07":
                	       	    echo "Stradivari 1709 King Maximilian";
-			    echo "<br>"
+			    echo "<br>";
         	    	    break;
-		       case 'D08':
+		       case "D08":
                	       	    echo "Stradivari 1715 Baron Knoop";
-			    echo "<br>"
+			    echo "<br>";
         	    	    break;
-		       case 'D09':
+		       case "D09":
                	       	    echo "Stradivari 1722 Jupiter";
-			    echo "<br>"
+			    echo "<br>";
         	    	    break;
-		       case 'D10':
+		       case "D10":
                	       	    echo "Stradivari 1723 Kiesewetter";
-			    echo "<br>"
+			    echo "<br>";
         	    	    break;
-	               case 'D11':
+	               case "D11":
                	       	    echo "Stradivari 1727 Dupont";
-			    echo "<br>"
+			    echo "<br>";
         	    	    break;
-		       case 'D12':
+		       case "D12":
                	       	    echo "Stradivari 1734 Willemotte";
-			    echo "<br>"
+			    echo "<br>";
         	    	    break;
-		       case 'D13':
+		       case "D13":
                	       	    echo "Stradivari 1736 Muntz";
-			    echo "<br>"
+			    echo "<br>";
         	    	    break;
 		}
 	}
-	
+
+        echo "<br>";
+        
         readfile('thankyou_footer.html');
         
-        exit();
         }
      ?>
